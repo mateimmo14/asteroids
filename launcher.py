4657f84e7a9d0ee9bdb8e5714c6a2b9a8da8c599
mateimmo14 <mateimmo14@gmail.com>
2026-01-16 23:30:26 +0200

    Added hard mode, delayed outline colors

--- launcher.py
+++ launcher.py
@@ -6,19 +6,27 @@ import time
 
 def main():
     root = tk.Tk()
-    def launch():
+    def launch_easy():
         root.destroy()
-        official_main.main()
-    def run():
+        official_main.main(False)
+    def run_easy():
         launch_button.configure(text="Launching...")
 
-        root.after(1000, launch)
+        root.after(1000, launch_easy)
+    def launch_hard():
+        root.destroy()
+        official_main.main(True)
+    def run_hard():
+        hard_launch_button.configure(text="Launching...")
+        root.after(1000, launch_hard)
 
 
     root.title("Asteroids launcher")
-    root.geometry("600x500")
+    root.geometry("700x600")
 
-    launch_button = tk.Button(root, text="Press to launch", bg="red",fg="black",font=("Times New Roman", 30, "bold"), command=run)
+    launch_button = tk.Button(root, text="Press to launch normal mode", bg="green",fg="white",font=("Sans Serif", 20, "bold"), command=run_easy)
     launch_button.pack(pady=80,ipady=100, ipadx=50)
+    hard_launch_button = tk.Button(root, text="Press to launch hard mode", bg="red",fg="white",font=("Sans Serif", 15, "bold"), command=run_hard)
+    hard_launch_button.pack(pady=10,ipady=25, ipadx=17.5)
     root.mainloop()
 main()
--- official_main.py
+++ official_main.py
@@ -10,10 +10,13 @@ from player import *
 from shoot import Shot
 
 
-def main():
+def main(difficulty):
     playtime = 0
     paused = False
-
+    if difficulty:
+        constants.ASTEROID_KINDS = 5
+        constants.ASTEROID_MIN_RADIUS = 30
+        constants.PLAYER_SHOOT_COOLDOWN_SECONDS = 0.3
     game_over = False
     score = 0
     pygame.init()
@@ -118,5 +121,4 @@ def main():
         dt = clock.tick(60) / 1000
 
 
-if __name__ == "__main__":
-    main()
+
