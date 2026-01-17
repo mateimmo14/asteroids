e06cf981b1700d0ca87e6d4926b2c957e72f0925
mateimmo14 <mateimmo14@gmail.com>
2026-01-17 11:45:22 +0200

    high score system, v2.1.1 bugfix

--- high_score.py
+++ high_score.py
@@ -1,10 +1,11 @@
-def high_score(score):
-    with open ("high_score.txt", "w+") as f:
-        f.seek(0)
-        high_score = f.read()
-        if high_score.strip() == "":
-            f.write(str(score))
-            return False
-        elif int(high_score.strip()) > score:
-            f.write(str(score))
-            return True
+import os
+import sys
+
+def get_high_score_path():
+    if getattr(sys, 'frozen', False):
+        # Running as PyInstaller executable
+        base_path = os.path.dirname(sys.executable)  # folder of the exe
+    else:
+        # Running as script
+        base_path = os.path.dirname(__file__)
+    return os.path.join(base_path, "high_score.txt")
